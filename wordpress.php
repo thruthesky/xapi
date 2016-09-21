990c35d4c02c41628b624de034c11ee3f76496f5
JaeHo Song <thruthesky@gmail.com>
2016-09-21 14:16:57 +0900

    Refactoring codes

--- wordpress.php
+++ wordpress.php
@@ -5,9 +5,13 @@
  */
 class XWordpress {
     public function wp_query() {
-        $ret = new stdClass();
         $args = isset($_REQUEST['args']) ? $_REQUEST['args'] : [];
         $data = new WP_Query( $args );
+        $data = $this->getWPQueryJsonData( $data );
+        wp_send_json_success($data);
+    }
+    public function getWPQueryJsonData( $data ) {
+        $ret = new stdClass();
         if ( $data && $data->posts ) {
             foreach ( $data->posts as $p ) {
                 unset(
@@ -29,12 +33,11 @@ class XWordpress {
                 );
                 $p->meta = get_post_meta( $p->ID );
             }
-
             $ret->found_posts = $data->found_posts;
             $ret->max_num_pages = $data->max_num_pages;
             $ret->posts = $data->posts;
             $ret->post_count = $data->post_count;
         }
-        wp_send_json_success($ret);
+        return $ret;
     }
 }
